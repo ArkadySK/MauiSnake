1189db68103d6ea053976c7fea262e688b5cdcf8
ArkadySK <adamkooo2@gmail.com>
2022-09-03 14:02:36 +0200

    created nullable integer to string converter

---
+++ Converters/NullableIntToString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiSnake.Converters
+{
+    internal class NullableIntToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is not null)
+                return value.ToString();
+
+            if (parameter is string resultText)
+                return resultText;
+            else
+                return "Null";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
